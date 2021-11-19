af1ad38e6607f7ba10f09be376f82972501b22ac
Aniket Singh <72797463+aniket2700@users.noreply.github.com>
2021-11-19 17:30:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function serviceUnavailable(){
+    alert('Service Unavailable')
+}
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "<sender’s email address>",
+	Password : "<email password>",
+	To : '<recipient’s email address>',
+	From : "<sender’s email address>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("Email Sent Successfully.")
+	);
+}
